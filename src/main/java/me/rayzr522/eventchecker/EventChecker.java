db455e2ec8bda253144c4ebba59123253ada8ed7
Rayzr522 <rayzr522@gmail.com>
2019-03-22 16:22:48 -0400

    First and probably last commit

---
+++ src/main/java/me/rayzr522/eventchecker/EventChecker.java
@@ -0,0 +1,85 @@
+package me.rayzr522.eventchecker;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class EventChecker extends JavaPlugin {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length < 1) {
+            return false;
+        }
+
+        String eventTypeName = args[0];
+
+        Class<? extends Event> eventClass;
+        try {
+            Class<?> rawClass = Class.forName(eventTypeName);
+
+            if (!Event.class.isAssignableFrom(rawClass)) {
+                sender.sendMessage(ChatColor.RED + "Only classes which extend the Event class are allowed!");
+                return true;
+            }
+
+            eventClass = rawClass.asSubclass(Event.class);
+        } catch (ClassNotFoundException e) {
+            sender.sendMessage(ChatColor.RED + "That class could not be found!");
+            return true;
+        }
+
+        Method getHandlerList;
+        try {
+            getHandlerList = eventClass.getMethod("getHandlerList");
+        } catch (NoSuchMethodException e) {
+            sender.sendMessage(ChatColor.RED + "Missing getHandlerList method on event type!");
+            return true;
+        }
+
+        HandlerList handlerList;
+        try {
+            handlerList = (HandlerList) getHandlerList.invoke(null);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            sender.sendMessage(ChatColor.RED + "Failed to get the handler list object!");
+            return true;
+        }
+
+        sender.sendMessage(String.format(
+                "%s%s----------%s Listeners for %s%s %s%s----------",
+                ChatColor.DARK_GRAY,
+                ChatColor.STRIKETHROUGH,
+                ChatColor.YELLOW,
+                ChatColor.RED,
+                eventClass.getSimpleName(),
+                ChatColor.DARK_GRAY,
+                ChatColor.STRIKETHROUGH
+        ));
+
+        Arrays.stream(handlerList.getRegisteredListeners())
+                .sorted(Comparator.comparing(provider -> provider.getPlugin().getName()))
+                .map(provider -> String.format(
+                        "%s%s%s - %s%s%s (%s)",
+
+                        ChatColor.GREEN,
+                        provider.getPlugin().getName(),
+                        ChatColor.YELLOW,
+
+                        ChatColor.RED,
+                        provider.getListener().getClass().getSimpleName(),
+                        ChatColor.YELLOW,
+
+                        provider.getPriority().name()
+                ))
+                .forEach(sender::sendMessage);
+
+        return true;
+    }
+}
