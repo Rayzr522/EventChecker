96cc6c15f15eabb31225f5708ae3c6eb1c8d65d5
Rayzr522 <rayzr522@gmail.com>
2019-04-19 14:57:27 -0400

    Improved ReflectionUtil to deduplicate classes

--- src/main/java/me/rayzr522/eventchecker/util/ReflectionUtil.java
+++ src/main/java/me/rayzr522/eventchecker/util/ReflectionUtil.java
@@ -9,16 +9,17 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class ReflectionUtil {
-    private static final Map<ClassLoader, List<Class<? extends Event>>> eventClassCache = new HashMap<>();
+    private static final Map<ClassLoader, List<Class<Event>>> eventClassCache = new HashMap<>();
 
     public static List<Class<? extends Event>> getAllEventClasses() {
         return Arrays.stream(Bukkit.getServer().getPluginManager().getPlugins())
                 .flatMap(plugin -> getEventClasses(plugin.getClass().getClassLoader()).stream())
+                .distinct()
                 .collect(Collectors.toList());
     }
 
     @SuppressWarnings({"unchecked", "UnstableApiUsage"})
-    public static List<Class<? extends Event>> getEventClasses(ClassLoader classLoader) {
+    public static List<Class<Event>> getEventClasses(ClassLoader classLoader) {
         return eventClassCache.computeIfAbsent(classLoader, loader -> {
             try {
                 return ClassPath.from(classLoader).getTopLevelClasses().stream()
@@ -39,7 +40,7 @@ public class ReflectionUtil {
                                 return false;
                             }
                         })
-                        .map(eventClass -> (Class<? extends Event>) eventClass)
+                        .map(eventClass -> (Class<Event>) eventClass)
                         .collect(Collectors.toList());
             } catch (IOException e) {
                 e.printStackTrace();
